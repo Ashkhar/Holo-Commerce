a5cb411b32459af921f8f350e6e127a28b0d2fed
Ashkhar Ahamed <ashkharahamed786@gmail.com>
2025-05-01 18:29:07 +0700

    Updated app.js and package.json, added .env to .gitignore

--- app.js
+++ app.js
@@ -9,17 +9,19 @@ const products = require("./products");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const app = express();
 
 // ===========================
 // 🛠 MongoDB Connection
 // ===========================
-mongoose.connect("mongodb://localhost:27017/holoCommerce", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
+
 
 // ===========================
 // ⚙️ Middleware
